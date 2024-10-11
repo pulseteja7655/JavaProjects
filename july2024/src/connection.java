5caa1a11a909ce82ecbc7eb0ea0150eb3363deb2
Projects <139626979+pulseteja7655@users.noreply.github.com>
2024-10-11 20:39:15 +0530

    Add files via upload

---
+++ july2024/src/connection.java
@@ -0,0 +1,15 @@
+import java.sql.Statement;
+
+public class connection {
+
+	public Statement createStatement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public PrepareStatement PrepareStatement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
